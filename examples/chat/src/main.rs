6c27e0cf1ea6434985fd140a9e8c1b1dfc510666
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-02-23 16:23:25 -0500

    chat-example:more prompt formatting and oridering

--- examples/chat/src/main.rs
+++ examples/chat/src/main.rs
@@ -33,7 +33,7 @@ use git2::Config;
 use git2::ErrorCode;
 use git2::Repository;
 
-use ureq::{Agent, AgentBuilder};
+use ureq::{Agent, AgentBuilder, Error as UreqError};
 
 use libp2p::{
     gossipsub, mdns, noise,
@@ -42,10 +42,14 @@ use libp2p::{
 };
 use tokio::{io, io::AsyncBufReadExt, select};
 use tracing_subscriber::EnvFilter;
-
+use std::env;
 use tracing_subscriber::fmt::format;
 
 use tracing_subscriber::fmt::format::Format;
+
+use env_logger::{Builder, Env};
+use log::{debug, error, info, trace, warn};
+use std::env::args;
 
 // We create a custom network behaviour that combines Gossipsub and Mdns.
 #[derive(NetworkBehaviour)]
@@ -56,9 +60,31 @@ struct MyBehaviour {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let _ = tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env())
-        .try_init();
+    //let _ = tracing_subscriber::fmt()
+    //    .with_env_filter(EnvFilter::from_default_env())
+    //    .try_init();
+
+    let args_vec: Vec<String> = env::args().collect();
+
+    if args_vec.len() < 2 {
+        info!("Please provide at least one argument.");
+        ()
+    }
+
+    if let Some(log_level) = args().nth(1) {
+        Builder::from_env(Env::default().default_filter_or(log_level)).init();
+    } else {
+        Builder::from_env(Env::default().default_filter_or("warn")).init();
+    }
+
+    trace!("Arguments:");
+    for (index, arg) in args_vec.iter().enumerate() {
+        if Some(index) == Some(0) {
+            trace!("Some(index) = Some(0):  {}: {}", index, arg);
+        } else {
+            trace!("  {}: {}", index, arg);
+        }
+    }
 
     let mut swarm = libp2p::SwarmBuilder::with_new_identity()
         .with_tokio()
@@ -78,9 +104,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
             // Set a custom gossipsub configuration
             let gossipsub_config = gossipsub::ConfigBuilder::default()
-                .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
-                .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message
-                // signing)
+                .heartbeat_interval(Duration::from_secs(1)) // This is set to aid debugging by not cluttering the log space
+                //REF https://docs.rs/gossipsub/latest/gossipsub/enum.ValidationMode.html
+                .validation_mode(gossipsub::ValidationMode::Permissive)
                 .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
                 .build()
                 .map_err(|msg| io::Error::new(io::ErrorKind::Other, msg))?; // Temporary hack because `build` does not return a proper `std::error::Error`.
@@ -105,22 +131,23 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let head = repo.head()?;
 
     // Print the name of HEAD (e.g., "refs/heads/main" or "HEAD")
-    println!("HEAD: {}", head.name().unwrap_or("HEAD"));
+    debug!("HEAD: {}", head.name().unwrap_or("HEAD"));
 
     // Get the commit object that HEAD points to
     let commit = head.peel_to_commit()?;
 
     // Print the commit ID (SHA-1 hash)
-    println!("Commit ID: {}", commit.id());
+    debug!("Commit ID: {}", commit.id());
 
     // Optionally, print other commit information
-    println!(
+    debug!(
         "Commit message: {}",
         commit.message().unwrap_or("No message")
     );
 
+    //TODO add cli topic arg
     let topic = gossipsub::IdentTopic::new(format!("{:0>64}", commit.id()));
-    println!("Topic: {:0>64}", topic);
+    println!("TOPIC> {:0>64}", topic);
     // subscribes to our topic
     swarm.behaviour_mut().gossipsub.subscribe(&topic)?;
 
@@ -131,23 +158,76 @@ async fn main() -> Result<(), Box<dyn Error>> {
     swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    println!("Enter messages via STDIN and they will be sent to connected peers using Gossipsub");
+    info!("Enter messages via STDIN and they will be sent to connected peers using Gossipsub");
 
+    let mut mempool_url = "https://mempool.space/api/blocks/tip/height";
+    let sweetsats_url = "https://mempool.sweetsats.io/api/blocks/tip/height";
+    let gob_sv_url = "https://bitcoin.gob.sv/api/blocks/tip/height";
+
+    let mut handles = Vec::new();
+    let ureq_test = tokio::spawn(async move {
+        match ureq::get(mempool_url).call() {
+            Ok(response) => {
+                /* it worked */
+
+                debug!("{response:?}");
+            }
+            Err(UreqError::Status(code, response)) => {
+                debug!("{response:?}");
+                mempool_url = sweetsats_url;
+                //mempool_url = gob_sv_url;
+                /* the server returned an unexpected status
+                code (such as 400, 500 etc) */
+                error!("{mempool_url:?}/{code:?}/{response:?}");
+            }
+            Err(_) => { /* some kind of io/transport error */ }
+        }
+    });
+    handles.push(ureq_test);
+    for i in handles {
+        i.await.unwrap();
+    }
+
+    let mut handles = Vec::new();
+    let ureq_test = tokio::spawn(async move {
+        match ureq::get(mempool_url).call() {
+            Ok(response) => {
+                /* it worked */
+
+                debug!("{response:?}");
+            }
+            Err(UreqError::Status(code, response)) => {
+                debug!("{response:?}");
+                //mempool_url = sweetsats_url;
+                mempool_url = gob_sv_url;
+                /* the server returned an unexpected status
+                code (such as 400, 500 etc) */
+                error!("{mempool_url:?}/{code:?}/{response:?}");
+            }
+            Err(_) => { /* some kind of io/transport error */ }
+        }
+    });
+    handles.push(ureq_test);
+    for i in handles {
+        i.await.unwrap();
+    }
+
+    let mut handles = Vec::new();
     let s = tokio::spawn(async move {
         let agent: Agent = ureq::AgentBuilder::new()
-            .timeout_read(Duration::from_secs(1))
-            .timeout_write(Duration::from_secs(1))
+            .timeout_read(Duration::from_secs(10))
+            .timeout_write(Duration::from_secs(10))
             .build();
         let body: String = agent
-            .get("https://mempool.sweetsats.io/api/blocks/tip/height")
+            .get(mempool_url)
             .call()
             .expect("")
             .into_string()
-            .expect("");
+            .expect("mempool_url:body:into_string:fail!");
 
         print!("{body}> ",)
     });
-    let mut handles = Vec::new();
+    //let mut handles = Vec::new();
     handles.push(s);
 
     for i in handles {
@@ -155,129 +235,166 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
     // Kick it off
     loop {
-        let s = tokio::spawn(async move {
-            let agent: Agent = ureq::AgentBuilder::new()
-                .timeout_read(Duration::from_secs(1))
-                .timeout_write(Duration::from_secs(1))
-                .build();
-            let body: String = agent
-                .get("https://mempool.sweetsats.io/api/blocks/tip/height")
-                .call()
-                .expect("")
-                .into_string()
-                .expect("");
-
-            //print!("\n170:{body}> ",);
+        let mut handles = Vec::new();
+        let ureq_test = tokio::spawn(async move {
+            match ureq::get(mempool_url).call() {
+                Ok(response) => {
+                    /* it worked */
+                    debug!("{response:?}");
+                }
+                Err(UreqError::Status(code, response)) => {
+                    debug!("{response:?}");
+                    mempool_url = sweetsats_url;
+                    //mempool_url = gob_sv_url;
+                    /* the server returned an unexpected status
+                    code (such as 400, 500 etc) */
+                    error!("{mempool_url:?}/{code:?}/{response:?}");
+                }
+                Err(_) => { /* some kind of io/transport error */ }
+            }
         });
+        handles.push(ureq_test);
+        for i in handles {
+            i.await.unwrap();
+        }
 
-        //let mut handles = Vec::new();
-        //handles.push(s);
+        let mut handles = Vec::new();
+        let ureq_test = tokio::spawn(async move {
+            match ureq::get(mempool_url).call() {
+                Ok(response) => {
 
-        //for i in handles {
-        //    i.await.unwrap();
-        //}
+                    debug!("{response:?}");
+                }
+                Err(UreqError::Status(code, response)) => {
+                    //debug!("{response:?}");
+                    //mempool_url = sweetsats_url;
+                    mempool_url = gob_sv_url;
+                    /* the server returned an unexpected status
+                    code (such as 400, 500 etc) */
+                    error!("{mempool_url:?}/{code:?}/{response:?}");
+                }
+                Err(_) => { /* some kind of io/transport error */ }
+            }
+        });
+        handles.push(ureq_test);
+        for i in handles {
+            i.await.unwrap();
+        }
+
         select! {
             Ok(Some(line)) = stdin.next_line() => {
                 if let Err(e) = swarm
                     .behaviour_mut().gossipsub
+                    //SEND
                     .publish(topic.clone(), line.as_bytes()) {
 
-                let s = tokio::spawn(async move {
-                    let agent: Agent = ureq::AgentBuilder::new()
-                        .timeout_read(Duration::from_secs(1))
-                        .timeout_write(Duration::from_secs(1))
-                        .build();
-                    let body: String = agent
-                        .get("https://mempool.sweetsats.io/api/blocks/tip/height")
-                        .call()
-                        .expect("")
-                        .into_string()
-                        .expect("");
+                    //formatting for error prompt
+                    //let s = tokio::spawn(async move {
+                    //    let agent: Agent = ureq::AgentBuilder::new()
+                    //        .timeout_read(Duration::from_secs(1))
+                    //        .timeout_write(Duration::from_secs(1))
+                    //        .build();
+                    //    let body: String = agent
+                    //        .get(mempool_url)
+                    //        .call()
+                    //        .expect("")
+                    //        .into_string()
+                    //        .expect("");
 
-                    //print!("\n197:{body}> {e:?}",);
-                });
-                let mut handles = Vec::new();
-                handles.push(s);
+                    //    print!("\n304:{body}> {e:?}",);
+                    //});
+                    //let mut handles = Vec::new();
+                    //handles.push(s);
 
-                //for i in handles {
-                //    i.await.unwrap();
-                //}
-
-
-            }
-
-                let s = tokio::spawn(async move {
-                    let agent: Agent = ureq::AgentBuilder::new()
-                        .timeout_read(Duration::from_secs(1))
-                        .timeout_write(Duration::from_secs(1))
-                        .build();
-                    let body: String = agent
-                        .get("https://mempool.sweetsats.io/api/blocks/tip/height")
-                        .call()
-                        .expect("")
-                        .into_string()
-                        .expect("");
-
-                    print!("\n221:{body}> ");
-                });
-                let mut handles = Vec::new();
-                handles.push(s);
-
-                for i in handles {
-                    i.await.unwrap();
+                    //for i in handles {
+                    //    i.await.unwrap(); //write to term
+                    //}
                 }
 
+            //let s = tokio::spawn(async move {
+            //    let agent: Agent = ureq::AgentBuilder::new()
+            //        .timeout_read(Duration::from_secs(1))
+            //        .timeout_write(Duration::from_secs(1))
+            //        .build();
+            //    let body: String = agent
+            //        .get("https://mempool.space/api/blocks/tip/height")
+            //        .call()
+            //        .expect("")
+            //        .into_string()
+            //        .expect("");
 
+            //    print!("\n326:{body}> ");
+            //});
+            //let mut handles = Vec::new();
+            //handles.push(s);
+
+            //for i in handles {
+            //    i.await.unwrap();
+            //}
+
+
+            //}
+
+            //let s = tokio::spawn(async move {
+            //    let agent: Agent = ureq::AgentBuilder::new()
+            //        .timeout_read(Duration::from_secs(1))
+            //        .timeout_write(Duration::from_secs(1))
+            //        .build();
+            //    let body: String = agent
+            //        .get(mempool_url)
+            //        .call()
+            //        .expect("")
+            //        .into_string()
+            //        .expect("");
+
+            //    print!("\n350:{body}> ");
+            //});
+            //let mut handles = Vec::new();
+            //handles.push(s);
+
+            //for i in handles {
+            //    i.await.unwrap();
+            //}
 
             }
             event = swarm.select_next_some() => match event {
                 SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
                     for (peer_id, _multiaddr) in list {
-                        print!("mDNS discovered a new peer: {peer_id}");
+                        debug!("mDNS discovered a new peer: {peer_id}");
                         swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
                     }
                 },
                 SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
                     for (peer_id, _multiaddr) in list {
-                        print!("mDNS discover peer has expired: {peer_id}");
+                        debug!("mDNS discover peer has expired: {peer_id}");
                         swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer_id);
                     }
                 },
+                //we recieve message
+                //and print immediately
+                //no formatting
                 SwarmEvent::Behaviour(MyBehaviourEvent::Gossipsub(gossipsub::Event::Message {
                     propagation_source: peer_id,
                     message_id: id,
                     message,
                 })) => {
-                    let mut handles = Vec::new();
-                    let r = tokio::spawn(async move {
-                        let agent: Agent = ureq::AgentBuilder::new()
-                            .timeout_read(Duration::from_secs(1))
-                            .timeout_write(Duration::from_secs(1))
-                            .build();
-                        let body: String = agent.get("https://mempool.sweetsats.io/api/blocks/tip/height")
-                            .call().expect("")
-                            .into_string().expect("");
 
-                        print!(
-                            "{}", String::from_utf8_lossy(&message.data),
-                        )
-                    });
-                    handles.push(r);
-                    for i in handles {
-                        i.await.unwrap();
-                    }
+                      print!(
+                          "{}", String::from_utf8_lossy(&message.data),
+                      );
 
                     let s = tokio::spawn(async move {
                         let agent: Agent = ureq::AgentBuilder::new()
                             .timeout_read(Duration::from_secs(1))
                             .timeout_write(Duration::from_secs(1))
                             .build();
-                        let body: String = agent.get("https://mempool.sweetsats.io/api/blocks/tip/height")
+                        let body: String = agent.get(mempool_url)
                             .call().expect("")
                             .into_string().expect("");
 
+                        //immediately print a new prompt
                         print!(
-                            "\n214:{body}> ",
+                            "\n397:{peer_id}/{id}/{body}>>> ",
                         )
                     });
 
@@ -294,12 +411,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
                             .timeout_read(Duration::from_secs(1))
                             .timeout_write(Duration::from_secs(1))
                             .build();
-                        let body: String = agent.get("https://mempool.sweetsats.io/api/blocks/tip/height")
+                        let body: String = agent.get(mempool_url)
                             .call().expect("")
                             .into_string().expect("");
 
                         print!(
-                            "\n256:{body}> ",
+                            "\n419:{body}> ",
                         )
                     });
 
@@ -310,9 +427,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         i.await.unwrap();
                     }
 
-
-
-                    print!("Local node is listening on {address}");
+                    info!("Local node is listening on {address}");
                 }
                 _ => {}
             }
