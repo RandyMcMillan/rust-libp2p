7f4cd617fb9e8f45b7bcbd0dcda2bf05eedf77d7
randymcmillan <randymcmillan@protonmail.com>
2025-08-14 08:52:39 -0400

    src:intermediate:building

--- examples/file-sharing/src/main.rs
+++ examples/file-sharing/src/main.rs
@@ -1,45 +1,27 @@
-// Copyright 2021 Protocol Labs.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
-
 #![doc = include_str!("../README.md")]
 
 mod network;
 
+use async_std::io;
 use clap::Parser;
-use futures::prelude::*;
-use futures::StreamExt;
-use libp2p::{core::Multiaddr, kad, kad::store::MemoryStore, kad::Mode, multiaddr::Protocol};
+use futures::{prelude::*, select, StreamExt};
+use libp2p::{
+    core::Multiaddr,
+    kad,
+    kad::store::MemoryStore,
+    kad::Mode,
+    mdns,
+    multiaddr::Protocol,
+    noise,
+    swarm::{NetworkBehaviour, SwarmEvent},
+    tcp, yamux,
+};
 use std::error::Error;
 use std::io::Write;
 use std::path::PathBuf;
 use tokio::task::spawn;
 use tokio::time::Duration;
 use tracing_subscriber::EnvFilter;
-
-use async_std::io;
-use futures::{prelude::*, select};
-use libp2p::{
-    mdns, noise,
-    swarm::{NetworkBehaviour, SwarmEvent},
-    tcp, yamux,
-};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
